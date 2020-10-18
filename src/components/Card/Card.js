a32f04dd7708f7ddf4397ea9f0d606878c79444c
Mariusz Gubernat <mariusz.gubernat@protonmail.com>
2020-10-19 01:19:28 +0200

    Adds some fixes

---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div className={styles.component}>
+                <h3>{this.props.title}</h3>
+            </div>
+        )
+    }
+}
+
+export default Card
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Column.scss';
+import Card from '../Card/Card';
 
 class Column extends React.Component {
+
+    state = {
+        cards: this.props.cards || [],
+    }
+
     render() {
         return (
             <section className={styles.component}>
                 <h3 className={styles.title}>{this.props.title}</h3>
-            </section>
+                <div>{this.state.cards.map(({ key, ...cardsProps }) => (
+                    <Card key={key} {...cardsProps} />
+                ))}</div>
+            </section >
         )
     }
 }
