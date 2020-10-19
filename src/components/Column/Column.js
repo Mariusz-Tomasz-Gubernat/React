449447c3d5daf11c50b00d41bd75a6c62dbc066e
Mariusz Gubernat <mariusz.gubernat@protonmail.com>
2020-10-19 02:55:44 +0200

    Complete first version component Column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,20 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Column.scss';
 import Card from '../Card/Card';
+import Creator from '../Creator/Creator'
+import { listData, settings } from '../../data/dataStore';
+import Icon from '../Icon/Icon'
 
 class Column extends React.Component {
 
     state = {
         cards: this.props.cards || [],
+        icon: this.props.icon.name
     }
 
+    addCard(title) {
+        this.setState(state => (
+            {
+                cards: [
+                    ...state.cards,
+                    {
+                        key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+                        title,
+                    }
+                ]
+            }
+        ));
+    }
     render() {
+        const { icon } = this.props;
         return (
             <section className={styles.component}>
-                <h3 className={styles.title}>{this.props.title}</h3>
-                <div>{this.state.cards.map(({ key, ...cardsProps }) => (
-                    <Card key={key} {...cardsProps} />
-                ))}</div>
+                <h3 className={styles.title}>
+                    {this.props.title}
+                    <span className={styles.icon}>
+                        <Icon name={icon} />
+                    </span>
+                </h3>
+
+                <div>
+                    {this.state.cards.map(({ key, ...cardsProps }) => (
+                        <Card key={key} {...cardsProps} />
+                    ))}
+                </div>
+
+                <div className={styles.creator}>
+                    <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+                </div>
+
             </section >
         )
     }
